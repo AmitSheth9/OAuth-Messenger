3d9235986f8633d67e62b273e8e6475ca0dfe5d7
AmitSheth9 <amit.sheth9@gmail.com>
2022-02-10 13:56:43 -0600

    wrote sql tables, and oauth login routes and util functions

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE github_users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL,
+    avatar TEXT
+);
+
+CREATE TABLE posts (
+    post_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    message TEXT,
+    github_id BIGINT REFERENCES github_users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
